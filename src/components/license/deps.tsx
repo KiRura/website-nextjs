c899a08776f98cf014f961dc1624652b84698c53
KiRura <57588049+KiRura@users.noreply.github.com>
2025-02-28 18:35:11 +0900

    アニメーション付きgriditemをコンポーネント化

--- src/components/license/deps.tsx
+++ src/components/license/deps.tsx
@@ -1,36 +1,42 @@
 import deps from "@/app/license/licenses.json";
-import { Card, Code, Flex, GridItem, Link, Tag } from "@chakra-ui/react";
+import { Card, Code, Flex, Link, Tag } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { AnimatedGridItem } from "../animated_griditem";
 
 const regex =
 	/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 export function Deps() {
-	return deps.map((dep) => {
-		const link = (dep.link.match(regex) || [dep.link])[0];
+	return (
+		<AnimatedGridItem
+			elementArray={deps.map((dep, _i) => {
+				const link = (dep.link.match(regex) || [dep.link])[0];
 
-		return (
-			<GridItem key={dep.name}>
-				<Card.Root size="sm">
-					<Card.Body gap={4}>
-						<Flex align="start" justify="space-between">
-							<Card.Title asChild>
-								<Link asChild variant="underline">
-									<NextLink href={link} target="_blank">
-										{dep.name}
-									</NextLink>
-								</Link>
-							</Card.Title>
-							<Code>{dep.installedVersion}</Code>
-						</Flex>
-						<Flex>
-							<Tag.Root>
-								<Tag.Label>{dep.licenseType}</Tag.Label>
-							</Tag.Root>
-						</Flex>
-					</Card.Body>
-				</Card.Root>
-			</GridItem>
-		);
-	});
+				return {
+					key: dep.name,
+					element: (
+						<Card.Root size="sm">
+							<Card.Body gap={4}>
+								<Flex align="start" justify="space-between">
+									<Card.Title asChild>
+										<Link asChild variant="underline">
+											<NextLink href={link} target="_blank">
+												{dep.name}
+											</NextLink>
+										</Link>
+									</Card.Title>
+									<Code>{dep.installedVersion}</Code>
+								</Flex>
+								<Flex>
+									<Tag.Root>
+										<Tag.Label>{dep.licenseType}</Tag.Label>
+									</Tag.Root>
+								</Flex>
+							</Card.Body>
+						</Card.Root>
+					),
+				};
+			})}
+		/>
+	);
 }
--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,104 +1,82 @@
 "use client";
 
-import { Card, Flex, GridItem, HStack, Link } from "@chakra-ui/react";
+import { Card, Flex, HStack, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { AnimatedGridItem } from "./animated_griditem";
 import { links } from "./linksdata";
 import { Tooltip } from "./ui/tooltip";
 
 export function Links() {
 	const [copied, setCopied] = useState(-1);
 	const [errored, setErrored] = useState(-1);
-	const [showedIndex, setShowedIndex] = useState(-1);
 
-	useEffect(() => {
-		const intervalId = setInterval(
-			() => {
-				setShowedIndex((i) => {
-					console.log(i);
-					if (i >= links.length - 1) {
-						clearInterval(intervalId);
-						return i;
-					}
-					return i + 2;
-				});
-			},
-			(1 / 120) * 1000,
-		);
-
-		return () => {
-			clearInterval(intervalId);
-		};
-	}, []);
-
-	return links.map((link, i) => (
-		<GridItem
-			key={link.name}
-			visibility={showedIndex >= i ? "visible" : "hidden"}
-			data-state={showedIndex >= i ? "open" : "closed"}
-			_open={{
-				animation: "ease-out",
-				animationName: "scale-in, fade-in",
-				animationDuration: "faster",
-			}}
-		>
-			<Card.Root size="sm" h="100%" variant="subtle" borderWidth={1}>
-				<Card.Body>
-					<Flex align="start" justify="space-between">
-						<HStack mb={2}>
-							<link.icon />
-							<Card.Title>
-								{link.href ? (
-									<Link asChild variant="underline">
-										<NextLink href={link.href} target="_blank">
-											{link.name}
-										</NextLink>
-									</Link>
-								) : (
-									link.name
-								)}
-							</Card.Title>
-						</HStack>
-						<Tooltip
-							content={
-								copied === i
-									? "コピーされました"
-									: errored === i
-										? "コピーできませんでした"
-										: "コピー"
-							}
-							showArrow
-							positioning={{ placement: "top" }}
-							onExitComplete={() => {
-								setCopied(-1);
-								setErrored(-1);
-							}}
-							closeOnPointerDown={false}
-							closeOnClick={false}
-							openDelay={0}
-						>
-							<Link
-								fontStyle="italic"
-								colorPalette="gray"
-								fontSize="sm"
-								overflowWrap="anywhere"
-								ml={2}
-								onClick={() => {
-									try {
-										navigator.clipboard.writeText(link.accountId);
-										setCopied(i);
-									} catch (_) {
-										setErrored(i);
-									}
-								}}
-							>
-								{link.accountId}
-							</Link>
-						</Tooltip>
-					</Flex>
-					<Card.Description>{link.description}</Card.Description>
-				</Card.Body>
-			</Card.Root>
-		</GridItem>
-	));
+	return (
+		<AnimatedGridItem
+			elementArray={links.map((link, i) => {
+				return {
+					key: link.name,
+					element: (
+						<Card.Root size="sm" h="100%" variant="subtle" borderWidth={1}>
+							<Card.Body>
+								<Flex align="start" justify="space-between">
+									<HStack mb={2}>
+										<link.icon />
+										<Card.Title>
+											{link.href ? (
+												<Link asChild variant="underline">
+													<NextLink href={link.href} target="_blank">
+														{link.name}
+													</NextLink>
+												</Link>
+											) : (
+												link.name
+											)}
+										</Card.Title>
+									</HStack>
+									<Tooltip
+										content={
+											copied === i
+												? "コピーされました"
+												: errored === i
+													? "コピーできませんでした"
+													: "コピー"
+										}
+										showArrow
+										positioning={{ placement: "top" }}
+										onExitComplete={() => {
+											setCopied(-1);
+											setErrored(-1);
+										}}
+										closeOnPointerDown={false}
+										closeOnClick={false}
+										openDelay={0}
+									>
+										<Link
+											fontStyle="italic"
+											colorPalette="gray"
+											fontSize="sm"
+											overflowWrap="anywhere"
+											ml={2}
+											onClick={() => {
+												try {
+													navigator.clipboard.writeText(link.accountId);
+													setCopied(i);
+												} catch (_) {
+													setErrored(i);
+												}
+											}}
+										>
+											{link.accountId}
+										</Link>
+									</Tooltip>
+								</Flex>
+								<Card.Description>{link.description}</Card.Description>
+							</Card.Body>
+						</Card.Root>
+					),
+				};
+			})}
+		/>
+	);
 }
