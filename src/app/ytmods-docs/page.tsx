bd39845f94df7bb195c6cf7e912367e8dc07b258
KiRura <57588049+KiRura@users.noreply.github.com>
2025-03-14 04:01:30 +0900

    ytmods-docsのcontainerにアニメーションを付加

--- src/app/ytmods-docs/page.tsx
+++ src/app/ytmods-docs/page.tsx
@@ -1,9 +1,10 @@
 import { MarkdownToComponent } from "@/components/ytmods-docs/markdownToComponent";
+import { config } from "@/config";
 import { Container, VStack } from "@chakra-ui/react";
 
 export default function Page() {
 	return (
-		<Container maxW="8xl">
+		<Container maxW="8xl" {...config.transitionAnimation}>
 			<VStack my={8}>
 				<MarkdownToComponent />
 			</VStack>
