73dadea9502f6665bfe6ccc1d6528209ee200ecb
KiRura <57588049+KiRura@users.noreply.github.com>
2025-04-27 01:28:49 +0900

    loadingとnot-foundの追加

---
+++ src/app/loading.tsx
@@ -0,0 +1,24 @@
+import { config } from "@/config";
+import { Container, EmptyState, Spinner } from "@chakra-ui/react";
+
+export default function Loading() {
+	return (
+		<Container as="main" maxW="8xl" minH="vh" {...config.transitionAnimation}>
+			<EmptyState.Root>
+				<EmptyState.Content>
+					<EmptyState.Indicator>
+						<Spinner />
+					</EmptyState.Indicator>
+					<EmptyState.Title
+						fontFamily="mono"
+						fontWeight="black"
+						fontSize="xl"
+						color="fg.muted"
+					>
+						Loading...
+					</EmptyState.Title>
+				</EmptyState.Content>
+			</EmptyState.Root>
+		</Container>
+	);
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import { config } from "@/config";
+import { Container, EmptyState, VStack } from "@chakra-ui/react";
+import { FaXmark } from "react-icons/fa6";
+
+export default function NotFound() {
+	return (
+		<Container as="main" maxW="8xl" minH="vh" {...config.transitionAnimation}>
+			<EmptyState.Root>
+				<EmptyState.Content gap={20}>
+					<EmptyState.Indicator>
+						<FaXmark />
+					</EmptyState.Indicator>
+					<VStack textAlign="center" gap={20}>
+						<EmptyState.Title
+							fontFamily="mono"
+							fontWeight="black"
+							fontSize="9xl"
+							color="fg.muted"
+						>
+							404
+						</EmptyState.Title>
+						<EmptyState.Description>ここはどこ お前は誰</EmptyState.Description>
+					</VStack>
+				</EmptyState.Content>
+			</EmptyState.Root>
+		</Container>
+	);
+}
