aa62cc8b6acb6a5f81c8eed9becccdcc9b1e7362
KiRura <57588049+KiRura@users.noreply.github.com>
2025-05-04 01:52:56 +0900

    colormodebuttonをカスタム

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -5,7 +5,7 @@ import { ClientOnly, IconButton, Skeleton, Span } from "@chakra-ui/react";
 import { ThemeProvider, useTheme } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 import * as React from "react";
-import { LuMoon, LuSun } from "react-icons/lu";
+import { FaMoon, FaSun } from "react-icons/fa6";
 
 export interface ColorModeProviderProps extends ThemeProviderProps {}
 
@@ -42,7 +42,7 @@ export function useColorModeValue<T>(light: T, dark: T) {
 
 export function ColorModeIcon() {
 	const { colorMode } = useColorMode();
-	return colorMode === "dark" ? <LuMoon /> : <LuSun />;
+	return colorMode === "dark" ? <FaMoon /> : <FaSun />;
 }
 
 interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
@@ -53,20 +53,13 @@ export const ColorModeButton = React.forwardRef<
 >(function ColorModeButton(props, ref) {
 	const { toggleColorMode } = useColorMode();
 	return (
-		<ClientOnly fallback={<Skeleton boxSize="8" />}>
+		<ClientOnly fallback={<Skeleton boxSize={10} />}>
 			<IconButton
 				onClick={toggleColorMode}
 				variant="ghost"
 				aria-label="Toggle color mode"
-				size="sm"
 				ref={ref}
 				{...props}
-				css={{
-					_icon: {
-						width: "5",
-						height: "5",
-					},
-				}}
 			>
 				<ColorModeIcon />
 			</IconButton>
