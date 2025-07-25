01c7b66377d8c27058f71ef3911c4075e6ed4a05
KiRura <57588049+KiRura@users.noreply.github.com>
2025-07-25 17:21:09 +0900

    フッターを弄った
    相変わらずお蔵入り

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,14 @@ import {
 	Box,
 	Container,
 	Flex,
+	HStack,
+	Image,
 	Link,
 	StackSeparator,
+	Text,
 	VStack,
 } from "@chakra-ui/react";
+import NextImage from "next/image";
 import NextLink from "next/link";
 import { FaUpRightFromSquare } from "react-icons/fa6";
 import { links } from "./linksdata";
@@ -13,8 +17,8 @@ import { links } from "./linksdata";
 export function Footer() {
 	return (
 		<Box borderTopWidth={1}>
-			<Container maxW="8xl" py="12">
-				<Flex>
+			<Container maxW="8xl" pt="12" pb="24">
+				<Flex justify="space-between" align="end">
 					<VStack
 						borderWidth={1}
 						rounded="md"
@@ -42,8 +46,9 @@ export function Footer() {
 										<Link
 											key={link.href}
 											asChild
-											my="2"
-											mx="3"
+											py="2"
+											px="3"
+											w="full"
 											color={{ base: "fg.muted", _hover: "orange.fg" }}
 										>
 											<NextLink href={link.href} target="_blank">
@@ -54,6 +59,25 @@ export function Footer() {
 								}),
 						]}
 					</VStack>
+					<NextLink href="/">
+						<HStack gap="2">
+							<Image
+								asChild
+								rounded="full"
+								boxSize={{ smDown: "12", sm: "16" }}
+							>
+								<NextImage
+									src="/kirura.png"
+									alt="kirura icon"
+									width={400}
+									height={400}
+								/>
+							</Image>
+							<Text fontSize={{ smDown: "4xl", sm: "5xl" }} fontWeight="bold">
+								KiRura
+							</Text>
+						</HStack>
+					</NextLink>
 				</Flex>
 			</Container>
 		</Box>
