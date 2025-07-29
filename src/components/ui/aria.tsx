670e6943369f5ea0818ece182aa4eeeef2db780c
KiRura <57588049+KiRura@users.noreply.github.com>
2025-07-29 21:39:09 +0900

    ariaのheadingがoverflowするのを修正
    カスの日本語過ぎて笑う

--- src/components/ui/aria.tsx
+++ src/components/ui/aria.tsx
@@ -12,11 +12,13 @@ export function Aria({
 }) {
 	return (
 		<VStack w="full" gap="4">
-			<HStack w="full">
+			<HStack w="full" overflow="hidden">
 				<Separator variant="dashed" borderColor="border.emphasized" flex="1" />
-				<HStack flexShrink="0" mx="2">
+				<HStack mx="2">
 					<Icon boxSize="6">{icon}</Icon>
-					<Heading size="2xl">{title}</Heading>
+					<Heading size="2xl" overflowWrap="anywhere">
+						{title}
+					</Heading>
 				</HStack>
 				<Separator variant="dashed" borderColor="border.emphasized" flex="1" />
 			</HStack>
