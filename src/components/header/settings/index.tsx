4cb4f11cf8dd61be2a7933605ba0ddfaf7a26c6e
KiRura <57588049+KiRura@users.noreply.github.com>
2025-10-06 22:54:36 +0900

    Safariに対する怨念 の微調整

--- src/components/header/settings/index.tsx
+++ src/components/header/settings/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
 	CloseButton,
 	Drawer,
@@ -40,7 +38,7 @@ export function Settings() {
 							<Text>
 								上記ブラウザエンジンにおけるデバッグ及び調整は一切行っておりません。如何なる視覚的なバグや挙動のバグも修正を行わず、あらゆる要望も受け付けません。
 								<br />
-								直近のSafariでフッターがアドレスバーに重なってしまうバグを確認していますが、Safari側の標準に従わない独自的な実装により発生しており、またそれに対する解決策も存在しません。
+								直近のSafariでフッターがアドレスバーに重なってしまうバグを確認していますが、Safari側の標準に従わない独自的な実装により発生しており、またそれに対する解決策も存在していません。
 							</Text>
 						</Drawer.Footer>
 						<Drawer.CloseTrigger asChild>
